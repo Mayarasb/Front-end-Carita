cd32ea0f3461df98151c2d2f29d3f79b1b70e537
Mayarasb <mayarasbarros7@gmail.com>
2025-09-01 22:35:17 -0300

    Atualização do projeto final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,11 @@ import { RouterOutlet,NavigationEnd,Router } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./componentes/footer/footer.component";
 import { AcessibilidadeComponent } from "./componentes/acessibilidade/acessibilidade.component";
-import { GraficoApexComponent } from './componentes/grafico/grafico.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, AcessibilidadeComponent, GraficoApexComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, AcessibilidadeComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
