766b72a94b805f766c7742cf26fcad0c9a86b519
arieleperes <ariele.peres@fatec.sp.gov.br>
2025-09-15 17:37:17 -0300

    gerando PDFs

--- src/app/componentes/como-ajudar/como-ajudar.component.ts
+++ src/app/componentes/como-ajudar/como-ajudar.component.ts
@@ -3,6 +3,7 @@ import { pontoArrecadacaoService } from '../../services/pontoArrecadacao.service
 import { MapaComponent } from "../mapa/mapa.component";
 import { pontoArrecadacao } from '../../models/pontosArrecadacao.model';
 import { CommonModule } from '@angular/common';
+import { jsPDF } from "jspdf";
 
 @Component({
   selector: 'app-como-ajudar',
@@ -21,4 +22,34 @@ export class ComoAjudarComponent  implements OnInit {
       this.pontosArrecadacao = dados;
     });
   }
+
+   gerarPDF() {
+  const doc = new jsPDF();
+  doc.setFontSize(16);
+  doc.text("Lista de Pontos de Arrecadação", 10, 10);
+  let y = 20; 
+
+  this.pontosArrecadacao.forEach((ponto, index) => {
+    const parceiro = ponto.parceiro?.nome ?? "Parceiro não informado";
+    const endereco = `${ponto.logradouro}, ${ponto.numero} – ${ponto.bairro}, ${ponto.cidade} - ${ponto.estado}, ${ponto.cep}`;
+
+    // Nome
+    doc.setFontSize(12);
+    doc.text(`${index + 1}. ${parceiro}`, 10, y);
+    y += 7;
+
+    // Endereço
+    doc.setFontSize(10);
+    doc.text(`Endereço: ${endereco}`, 15, y);
+    y += 12; 
+
+    // Quebra de página se precisar
+    if (y > 280) {
+      doc.addPage();
+      y = 20;
+    }
+  });
+  doc.save("pontos-arrecadacao.pdf");
 }
+}
+
